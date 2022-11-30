ff2c7e44aa63db0d8dcd110e0e717c0e8e407af6
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2022-11-30 22:12:49 +0100

    setup math event

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,33 +1,46 @@
 import React from 'react';
+import calculate from './logic/calculate';
 
 class CalcInterface extends React.Component { // eslint-disable-line
+  constructor(props) {
+    super(props);
+    this.state = {total: 0, next: null, operation: null}
+  }
+
+  handleClick = (event) => {
+    const buttonName = event.target.innerText;
+    const result = calculate(this.state, buttonName);
+    this.setState(result)
+  }
+
   render() {
+    const {total, next, operation } = this.state;
     return (
       <section>
         <div className="calc-container">
           <div className="header">
-            <input type="number" placeholder='0' className="p-tag" />
+            <button type="button" className="p-tag">{total}{operation}{next}</button>
           </div>
           <div className="calc-buttons">
-            <button type="button">Ac</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button" className="color">+</button>
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" className="color">*</button>
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" className="color">-</button>
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="color">+</button>
-            <button type="button" className="span">0</button>
-            <button type="button">.</button>
-            <button type="button" className="color">=</button>
+            <button type="submit" onClick={this.handleClick}>AC</button>
+            <button type="submit" onClick={this.handleClick}>+/-</button>
+            <button type="submit" onClick={this.handleClick}>%</button>
+            <button type="submit" onClick={this.handleClick} className="color">÷</button>
+            <button type="submit" onClick={this.handleClick}>7</button>
+            <button type="submit" onClick={this.handleClick}>8</button>
+            <button type="submit" onClick={this.handleClick}>9</button>
+            <button type="submit" onClick={this.handleClick} className="color">x</button>
+            <button type="submit" onClick={this.handleClick}>4</button>
+            <button type="submit" onClick={this.handleClick}>5</button>
+            <button type="submit" onClick={this.handleClick}>6</button>
+            <button type="submit" onClick={this.handleClick} className="color">-</button>
+            <button type="submit" onClick={this.handleClick}>1</button>
+            <button type="submit" onClick={this.handleClick}>2</button>
+            <button type="submit" onClick={this.handleClick}>3</button>
+            <button type="submit" onClick={this.handleClick} className="color">+</button>
+            <button type="submit" onClick={this.handleClick} className="span">0</button>
+            <button type="submit" onClick={this.handleClick}>.</button>
+            <button type="submit" onClick={this.handleClick} className="color">=</button>
           </div>
         </div>
       </section>
