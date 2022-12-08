63fc495fbabbea310936acbab76919fe0c0f73da
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2022-12-08 13:08:44 +0100

    run test for components

---
+++ src/__tests__/calculate.test.js
@@ -0,0 +1,26 @@
+import calculate from '../logic/calculate';
+
+describe('test on calculate.js function', () => {
+      test('when AC button is pressed', () => {
+        const output = calculate({
+          total: '8',
+          next: '6',
+          operation: '',
+        }, 'AC');
+        expect(output).toEqual({
+          total: null,
+          next: null,
+          operation: null,
+        })
+      });
+      test('= button', () => {
+        const output = calculate({
+          total: '5',
+          next: '5',
+          operation: '+'
+        }, '=');
+        expect(output.total).toBe('10');
+      });
+}); 
+
+//Component test
