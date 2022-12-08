ffd79f3302b39514ef5e360a8e33bacddfa0d43c
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2022-12-08 18:48:29 +0100

    fix linter error

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 import CalcInterface from '../components/calculator';
 
-describe("Test component", () => {
-    it("Renders correctly", () => {
-      const tree = render(<CalcInterface />);
-      expect(tree).toMatchSnapshot();
-    });
+describe('Test component', () => {
+  it('Renders correctly', () => {
+    const tree = render(<CalcInterface />);
+    expect(tree).toMatchSnapshot();
+  });
 });
--- src/__tests__/quote.test.js
+++ src/__tests__/quote.test.js
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
-import Quote from "../components/quote";
+import { render } from '@testing-library/react';
+import Quote from '../components/quote';
 
-describe("test navbar component", () => {
-  it("renders correctly", () => {
-    const tree = render(<Quote/>);
+describe('test navbar component', () => {
+  it('renders correctly', () => {
+    const tree = render(<Quote />);
     expect(tree).toMatchSnapshot();
   });
 });
